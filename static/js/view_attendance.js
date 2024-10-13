7cee9c596d66b19aaa123aaf26fe42f73ebf7384
Adrian Samuel <aeydeee@gmail.com>
2024-10-13 21:17:57 +0800

    removed the numbering in table

--- static/js/view_attendance.js
+++ static/js/view_attendance.js
@@ -6,17 +6,6 @@ const table = new DataTable('#view_attendance', {
             targets: 0
         }
     ],
-    order: [[3, 'desc']]
+    order: [[0, 'desc']]
 });
 
-table
-    .on('order.dt search.dt', function () {
-        let i = 1;
-
-        table
-            .cells(null, 0, {search: 'applied', order: 'applied'})
-            .every(function (cell) {
-                this.data(i++);
-            });
-    })
-    .draw();
